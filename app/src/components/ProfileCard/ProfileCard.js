1ab2fb1e3ea1b986d5e610f39f475aa3066d2907
sireax <sireax@yandex.ru>
2021-11-13 20:22:48 +0400

    cosmetic fixes 2

--- app/src/components/ProfileCard/ProfileCard.js
+++ app/src/components/ProfileCard/ProfileCard.js
@@ -9,12 +9,19 @@ export const ProfileCardAPI = (props) => {
         <div className={s.wrapper}>
             <div className={s.info}>
                 <div className={s.img}>
-                    <img src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"/>
+                    <img
+                        src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"/>
                 </div>
                 <div className={s.content}>
                     <p className={s.rating}>Рейтинг<span> 4.2</span></p>
                     <h3 className={'h3'}>Анна Андреева</h3>
                 </div>
+            </div>
+            <div className={s.stats}>
+                <ul>
+                    <li>Доставлено: 24 кг</li>
+                    <li>В проекте: 2 месяца</li>
+                </ul>
             </div>
             <ul>
                 <li>
